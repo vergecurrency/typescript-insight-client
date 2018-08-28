725a68c9b512ca20deb4787477462848b74fb3d7
marpme <marvin.piekarek5@gmail.com>
2018-08-28 18:34:56 +0200

    Interface segregation

--- IClient.ts
+++ IClient.ts
@@ -7,55 +7,61 @@ import { TransactionSummary } from './TransactionSummary';
 import { Info } from './Info';
 
 /*API HTTP Endpoints*/
-export interface IClient {
-  getBlock: (hash: number) => Block;
-  getBlocks: (date: Date, limit: number) => BlockSummary;
-  getBlockHashByIndex: (index: number) => number;
-  getBlockByIndex: (index: number) => Block;
-  // getRawBlock() - not needed, dunno
+export interface IClient
+  extends ITransactionMethods,
+    IBlockMethods,
+    IAccountMethods {
+  /**
+   * Broadcasts transaction and returns the transaction id
+   */
+  postTransaction: (rawTransaction: number) => Promise<number>;
 
-  getTransaction: (id: number) => Transaction;
-  getTransactionsByBlock: (blockhash: string) => Transaction[];
-  getTransactionsByAddress: (address: string) => Transaction[];
+  /**
+   * All infos in one
+   */
+  getInfo: () => Promise<Info>;
+  getDifficulty: () => Promise<number>;
+  getBestBlockHash: () => Promise<number>;
+  getLastBlockHash: () => Promise<number>;
+}
+
+export interface ITransactionMethods {
+  getTransaction: (id: number) => Promise<Transaction>;
+  getTransactionsByBlock: (blockhash: string) => Promise<Transaction[]>;
+  getTransactionsByAddress: (address: string) => Promise<Transaction[]>;
   getTransactionsByMultipleAddresses: (
     addresses: string[],
     from?: number,
     to?: number
-  ) => TransactionSummary;
-  // postTransactionsByMultipleAddresses - not really needed just more filter options
-  // getRawTransaction - not needed
+  ) => Promise<TransactionSummary>;
+}
 
+export interface IBlockMethods {
+  getBlock: (hash: number) => Promise<Block>;
+  getBlocks: (date: Date, limit: number) => Promise<BlockSummary>;
+  getBlockHashByIndex: (index: number) => Promise<number>;
+  getBlockByIndex: (index: number) => Promise<Block>;
+}
+
+export interface IAccountMethods {
   getAddressDetails: (
     address: string,
     showTransactions?: boolean, // it's 1 or 0
     from?: number, // number of transactions
     to?: number
-  ) => AddressSummary;
+  ) => Promise<AddressSummary>;
 
   // everything will return values in satoshis
-  getBalance: (address: string) => number;
-  getTotalReceived: (address: string) => number;
-  getTotalSent: (address: string) => number;
-  getUnconfirmedBalance: (address: string) => number;
-  getUnspentOutputs: (address: string) => Array<UnspentOutput>;
+  getBalance: (address: string) => Promise<number>;
+  getTotalReceived: (address: string) => Promise<number>;
+  getTotalSent: (address: string) => Promise<number>;
+  getUnconfirmedBalance: (address: string) => Promise<number>;
+
+  getUnspentOutputs: (address: string) => Promise<Array<UnspentOutput>>;
   getUnspentOutputsFormMultipleAddresses: (
     addresses: string[]
-  ) => Array<UnspentOutput>;
+  ) => Promise<Array<UnspentOutput>>;
   postUnspentOutputsFormMultipleAddresses: (
     addresses: string[]
-  ) => Array<UnspentOutput>;
-
-  /**
-   * Broadcasts transaction and returns the transaction id
-   */
-  postTransaction: (rawTransaction: number) => number;
-
-  /**
-   * All infos in one
-   */
-  getInfo: () => Info;
-
-  getDifficulty: () => number;
-  getBestBlockHash: () => number;
-  getLastBlockHash: () => number;
+  ) => Promise<Array<UnspentOutput>>;
 }
